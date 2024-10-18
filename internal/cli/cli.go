f800a2ac89de57abfbcd83fffa777df99a762110
GoldenMM <ryanjanderton@yahoo.com.au>
2024-10-18 16:45:03 +0800

    commit to properly import internals/cli

---
+++ internal/cli/cli.go
@@ -0,0 +1,9 @@
+package cli
+
+import (
+	"github.com/GoldenMM/blog-aggregator/internal/config"
+)
+
+type State struct {
+	cfg *config.Config
+}
--- main.go
+++ main.go
@@ -10,18 +10,9 @@ import (
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("unable to read config: %v", err)
+		log.Fatal(err)
 	}
+	s := State{cfg: cfg}
 
-	err = cfg.SetUser("ryan")
-	if err != nil {
-		log.Fatalf("unable to set user: %v", err)
-	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("unable to read config: %v", err)
-	}
-
-	fmt.Println(cfg)
+	fmt.Println(s)
 }
